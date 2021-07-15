65210fe39260f07cd62b5940bcc0dd5245c55140
natashaclerkin <natashaclerkin@gmail.com>
2021-07-15 02:10:53 +0000

    Customise reviews in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Review
 
 # Register your models here.
-admin.site.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'review_title',
+        'user_profile',
+        'created_on',
+    )
+
+    list_filter = ("product",)
+    search_fields = ['product', 'review_title', 'review_content']
+
+admin.site.register(Review, ReviewAdmin)
