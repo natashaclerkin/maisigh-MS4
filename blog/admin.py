f9cad57ea4e720d87013ec46dec76ef3c95d88ee
natashaclerkin <natashaclerkin@gmail.com>
2021-07-17 22:04:58 +0000

    Customise blog posts in admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,16 @@ from .models import BlogPost
 # Register your models here.
 
 
-admin.site.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'created_on',
+        'status',
+    )
+    list_filter = ("status",)
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
