978d96725b1cd71f59a489e7e5797c965844a15e
natashaclerkin <natashaclerkin@gmail.com>
2021-07-22 19:45:48 +0000

    Continue refactor contact form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
 
 from .forms import ContactForm
@@ -12,15 +11,11 @@ def contact(request):
         if form.is_valid():
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
-            try:
-                send_mail(message, from_email, ['maisighie@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found!')
             messages.success(request, (
                 "Thanks for getting in touch! "
                 "We will respond to you as soon as possible."
             ))
-            return redirect('home')
+        return redirect('home')
 
     context = {
         'form': form,
