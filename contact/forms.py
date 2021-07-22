72134390e58a25c18e2d647c0aa6082891cda924
natashaclerkin <natashaclerkin@gmail.com>
2021-07-22 18:31:13 +0000

    Test contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,21 +1,18 @@
 from django import forms
+from .models import Contact
 
 
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=20, required=True)
-    email = forms.EmailField(max_length=20, required=True)
-    message = forms.CharField(
-        max_length=2000,
-        widget=forms.Textarea(),
-        required=True
-    )
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'message')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
-    placeholders = {
-        'name': 'name',
-        'email': 'email',
-        'message': 'message',
-    }
+        placeholders = {
+            'name': 'Enter your Name',
+            'email': 'Enter you Email',
+            'message': 'Message',
+        }
--- contact/views.py
+++ contact/views.py
@@ -1,32 +1,43 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
 from django.conf import settings
-from .forms import ContactForm
 
-# Create your views here.
+from .forms import ContactForm
 
 
 def contact(request):
-    """ Customer contact form view """
-    contact_form = ContactForm
-    if request.method == "POST":
-        form = contact_form(data=request.POST)
 
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        customer_query = request.POST.get('message')
+        customer_email = request.POST.get('email')
+        customer_name = request.POST.get('full_name')
         if form.is_valid():
-            name = request.POST.get('name', '')
-            email = request.POST.get('email', '')
-            message = request.POST.get('message', '')
+            form.save()
 
-        send_mail(
-            name,
-            message,
-            email,
-            [settings.DEFAULT_FROM_EMAIL]
-        )
-        return redirect('contact')
+            send_mail(
+                'Maisigh.ie - New Contact Form',
+                'A new email has been received from:'
+                f'\n\n{customer_name}'
+                f'\n{customer_email}'
+                '\n\n\nCustomer Query:'
+                f'\n\n{customer_query}',
+                settings.DEFAULT_FROM_EMAIL,
+                [settings.DEFAULT_FROM_EMAIL],
+            )
+
+            messages.success(request, 'Email sent. We will be in touch soon!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(
+                request,
+                'Contact request failed. Check the form is valid')
+    else:
+        form = ContactForm()
+
     context = {
-        'form': contact_form,
-        }
+        'form': form
+    }
 
     return render(request, 'contact/contact.html', context)
